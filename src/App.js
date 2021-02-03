3e0ff6b1984f7e56bcee2122b76a85f96ff4c2f6
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-03 20:44:45 +0200

    add handleSearch fn

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-	useEffect(() => {
-		const token = `bearer ${process.env.REACT_APP_ACCESS_TOKEN}`
+	const [startDate, setStartDate] = useState('')
+	const [endDate, setEndDate] = useState('')
+	const [token, setToken] = useState('')
+
+	const changeStartDate = e => {
+		setStartDate(e.target.value)
+	}
+
+	const changeEndDate = e => {
+		setEndDate(e.target.value)
+	}
+
+	const changeToken = e => {
+		setToken(e.target.value)
+	}
+
+	const handleSearch = () => {
 		const config = {
-			headers: { Authorization: token },
+			headers: { Authorization: `bearer ${token}` },
 		}
 
 		axios
 			.get(
-				'https://api.giosg.com/api/reporting/v1/rooms/84e0fefa-5675-11e7-a349-00163efdd8db/chat-stats/daily/?start_date=2021-01-01&end_date=2021-01-30',
+				`https://api.giosg.com/api/reporting/v1/rooms/84e0fefa-5675-11e7-a349-00163efdd8db/chat-stats/daily/?start_date=${startDate}&end_date=${endDate}`,
 				config
 			)
 			.then(res => console.log('data', res.data))
-	}, [])
+
+		setStartDate('')
+		setEndDate('')
+		setToken('')
+	}
 
 	return (
 		<div className='App'>
 			<h2>Conversation Stat</h2>
 			<div>
 				<label htmlFor='start_date'>Start date</label>
-				<input id='start_date' />
+				<input id='start_date' value={startDate} onChange={changeStartDate} />
 				<label htmlFor='end_date'>End date</label>
-				<input id='end_date' />
-				<input id='token' placeholder='Access token' />
+				<input id='end_date' value={endDate} onChange={changeEndDate} />
+				<input
+					id='token'
+					placeholder='Access token'
+					value={token}
+					onChange={changeToken}
+				/>
+
+				<button onClick={handleSearch}>Search</button>
 			</div>
 		</div>
 	)
