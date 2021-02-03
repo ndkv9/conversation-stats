4f41e79a395f44a891f47686ed4be991f9021a88
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-03 20:21:41 +0200

    fetch API data with axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+
 const App = () => {
+	useEffect(() => {
+		const token = `bearer ${process.env.REACT_APP_ACCESS_TOKEN}`
+		const config = {
+			headers: { Authorization: token },
+		}
+
+		axios
+			.get(
+				'https://api.giosg.com/api/reporting/v1/rooms/84e0fefa-5675-11e7-a349-00163efdd8db/chat-stats/daily/?start_date=2021-01-01&end_date=2021-01-30',
+				config
+			)
+			.then(res => console.log('data', res.data))
+	}, [])
+
 	return (
 		<div className='App'>
 			<h2>Conversation Stat</h2>
 			<div>
-				<label for='start_date'>Start date</label>
+				<label htmlFor='start_date'>Start date</label>
 				<input id='start_date' />
-				<label for='end_date'>End date</label>
+				<label htmlFor='end_date'>End date</label>
 				<input id='end_date' />
 				<input id='token' placeholder='Access token' />
 			</div>
