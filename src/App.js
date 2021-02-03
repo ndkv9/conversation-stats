64992214d91b706f57fe526987d381a839999872
Vu Nguyen <vu.nguyen101@gmail.com>
2021-02-03 20:06:06 +0200

    add token to environment variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 const App = () => {
 	return (
 		<div className='App'>
-			<h2>AHIHI</h2>
+			<h2>Conversation Stat</h2>
+			<div>
+				<label for='start_date'>Start date</label>
+				<input id='start_date' />
+				<label for='end_date'>End date</label>
+				<input id='end_date' />
+				<input id='token' placeholder='Access token' />
+			</div>
 		</div>
 	)
 }
